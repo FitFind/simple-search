a111f797d62966a70e2c6572f67376c46ef73fa4
Jim Rosales <j.r99@hotmail.com>
2014-11-05 14:28:34 -0800

    Test

--- js/simpleSearch.js
+++ js/simpleSearch.js
@@ -19,6 +19,7 @@ $(function(){
 // query, and then injects results into the DOM
 // Output: void
 function search(query, $container, $template){
+alert("button works");
     $.ajax({
         type: 'GET',
         url: 'http://is-info320t1.ischool.uw.edu:8983/solr-example/collection1/select',
