4ec70140333eecaced9ccd0f5d360a9e7275c04d
sampaths2 <sampath2@uw.edu>
2014-11-16 16:17:00 -0800

    Update prof.js

--- prof.js
+++ prof.js
@@ -1,10 +1,5 @@
- <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
-	 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  	<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-	 <link rel="stylesheet" href="/resources/demos/style.css">
- 
  $(function() {
-    $( "#userform" ).dialog({
+    $( "#dialog" ).dialog({
       autoOpen: false,
       show: {
         effect: "blind",
@@ -16,7 +11,7 @@
       }
     });
  
-    $( "#userform" ).click(function() {
-      $( "#profile" ).dialog( "open" );
+    $( "#opener" ).click(function() {
+      $( "#dialog" ).dialog( "open" );
     });
   });
