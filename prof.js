3a63ff7c951006057f26207a49668ac02eb31123
sampaths2 <sampath2@uw.edu>
2014-11-16 16:00:26 -0800

    Update prof.js

--- prof.js
+++ prof.js
@@ -3,11 +3,12 @@ $(function() {
     $( "#dialog" ).dialog({
       autoOpen: false,
       show: {
-       
+       effect: "blind",
+        duration: 1000
       },
       hide: {
-      
-        
+      effect: "explode",
+       duration: 1000
       }
     });
  
