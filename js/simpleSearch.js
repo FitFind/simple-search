77807e670f6152b2b46b238d84ebf68a33d9c065
sampaths2 <sampath2@uw.edu>
2014-11-24 22:27:33 -0800

    Update simpleSearch.js

--- js/simpleSearch.js
+++ js/simpleSearch.js
@@ -21,6 +21,7 @@ $(function(){
 function search(query, $container, $template){
 alert("button works");
 document.querySelector(".container").style.display = "block";
+document.querySelector(".headcontent").style.padding = "15";
     $.ajax({
         type: 'GET',
         url: 'http://is-info320t1.ischool.uw.edu:8080/solr-example/collection1/select',
