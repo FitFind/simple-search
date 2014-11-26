d763cd0108bbb409ee119b621d3967413a893fc0
sampaths2 <sampath2@uw.edu>
2014-11-25 19:35:39 -0800

    Update simpleSearch.js

--- js/simpleSearch.js
+++ js/simpleSearch.js
@@ -53,7 +53,7 @@ document.querySelector("#headcontent").style.paddingBottom = "0";
 
 function renderSpellCheck(docs, $template) {
 	var result = document.getElementById("spellchecktemp")
-	result.innerHTML = "Did you mean:" + docs.suggestions[1].suggestion[0];
+	result.innerHTML = "Did you mean: " + docs.suggestions[1].suggestion[0];
 	 
 	} 
 
