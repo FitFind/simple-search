f00c1a89bbd4c80780e2cb14e455561fb1ae21bc
sampaths2 <sampath2@uw.edu>
2014-12-04 17:08:03 -0800

    Update simpleSearch.js

--- js/simpleSearch.js
+++ js/simpleSearch.js
@@ -43,7 +43,7 @@ document.querySelector("#headcontent").style.paddingBottom = "0";
         jsonp: 'json.wrf',
         success: function (data) {
             renderResults(data.response.docs, $container, $template);
-      	if (data.spellcheck != null) {
+      	if (data.spellcheck !== null) {
 				 
 		       renderSpellCheck(data.spellcheck, "#spellcheck#spellchecktemp");
       	}   
