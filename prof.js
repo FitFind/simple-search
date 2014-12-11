c329ba8f599ddfdb25504b7710d7ecc47b2dfb57
sampaths2 <sampath2@uw.edu>
2014-12-11 01:32:54 -0800

    Update prof.js

--- prof.js
+++ prof.js
@@ -1,1 +1,22 @@
-
+ $(function() {
+-    $( "#dialog" ).dialog({
+-      autoOpen: false,
+-      width: 500,
+-      show: {
+-       effect: "clip",
+-       duration: 100,
+-       
+-       
+-       
+-      },
+-      hide: {
+-        effect: "explode",
+-        duration: 100
+-      }
+-    });
+- 
+-    $( "#opener" ).click(function() {
+-      $( "#dialog" ).dialog( "open" );
+-      modal: true
+-    });
+-  });
