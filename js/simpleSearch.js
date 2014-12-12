bbcb244e86c005f67d79a8bfcda5e14c651e991b
sampaths2 <sampath2@uw.edu>
2014-12-11 16:30:39 -0800

    Update simpleSearch.js

--- js/simpleSearch.js
+++ js/simpleSearch.js
@@ -7,6 +7,7 @@ $(function(){
     
     function simpleSearch() {
     	var age = ": age " + document.getElementById("age").value;
+    	var sex = document.querySelector('input[name="sex"]:checked').value;
     	var query =  $( "input#query" ).val() + age;
     	var origquery = $( "input#query" ).val();
         search( origquery, query, $( "#results" ), $( ".template.result" ) );
